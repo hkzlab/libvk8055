275a0dbb578f2b3409b5d9f82c39f562b1faa8b3
Fabio Battaglia <hkzlabnet@gmail.com>
2012-01-15 19:44:38 +0100

    Formatting fix

--- src/k8055.c
+++ src/k8055.c
@@ -48,7 +48,7 @@ usb_dev_handle *get_k8055_board_handle(board_id id) {
 		for (cdev = cbus->devices; cdev != NULL; cdev = cdev->next) // Iterate over all devices in the bus
 			if (cdev->descriptor.idVendor == BOARD_VENDOR && cdev->descriptor.idProduct == id) {
 				board_handle = usb_open(cdev); // Found the board, opening it!
-	
+
 				if (claim_usb_device(board_handle, 0) < 0) {
 					fprintf(stderr, "K8055: Unable to claim board id 0x%.4X.\n", id);
 					usb_close(board_handle);
@@ -68,14 +68,14 @@ int read_k8055_status(usb_dev_handle *dev, k8055_data_packet *dp) {
 
 	memset(dp, 0, sizeof(k8055_data_packet));
 
-	return usb_interrupt_read(dev, READ_ENDPOINT, (char*)(dp->data), 8, DEFAULT_USB_TIMEOUT);
+	return usb_interrupt_read(dev, READ_ENDPOINT, (char *)(dp->data), 8, DEFAULT_USB_TIMEOUT);
 }
 
 int exec_k8055_command(usb_dev_handle *dev, k8055_data_packet *dp) {
 	assert(dev);
 	assert(dp);
 
-	return usb_interrupt_write(dev, WRITE_ENDPOINT, (char*)dp->data, 8, DEFAULT_USB_TIMEOUT);
+	return usb_interrupt_write(dev, WRITE_ENDPOINT, (char *)dp->data, 8, DEFAULT_USB_TIMEOUT);
 }
 
 void prepare_k8055_command(usb_dev_handle *dev, k8055_data_packet *dp, board_cmd cmd, unsigned char in1, unsigned char in2, unsigned char in3) {
@@ -85,37 +85,37 @@ void prepare_k8055_command(usb_dev_handle *dev, k8055_data_packet *dp, board_cmd
 	memset(dp, 0, sizeof(k8055_data_packet));
 
 	dp->data[0] = cmd;
-	
+
 	switch (cmd) {
-		case CMD_RESET:
-			break;
-		case CMD_SET_CNT1:
-			dp->data[6] = in1;
-			break;
-		case CMD_SET_CNT2:
-			dp->data[7] = in1;
-			break;
-		case CMD_RST_CNT1:
-			dp->data[4] = 0x00;
-			break;
-		case CMD_RST_CNT2:
-			dp->data[5] = 0x00;
-			break;
-		case CMD_SET_AD:
-			dp->data[1] = in1;
-			dp->data[2] = in2;
-			dp->data[3] = in3;
-			break;
-		default:
-			fprintf(stderr, "K8055: Unknown command 0x%.2X issued.\n", cmd);
-			break;
+	case CMD_RESET:
+		break;
+	case CMD_SET_CNT1:
+		dp->data[6] = in1;
+		break;
+	case CMD_SET_CNT2:
+		dp->data[7] = in1;
+		break;
+	case CMD_RST_CNT1:
+		dp->data[4] = 0x00;
+		break;
+	case CMD_RST_CNT2:
+		dp->data[5] = 0x00;
+		break;
+	case CMD_SET_AD:
+		dp->data[1] = in1;
+		dp->data[2] = in2;
+		dp->data[3] = in3;
+		break;
+	default:
+		fprintf(stderr, "K8055: Unknown command 0x%.2X issued.\n", cmd);
+		break;
 	}
 
 }
 
 // Debounce conversion formula took from libk8055 by Sven Lindberg! Thanks!
 unsigned char k8055_get_debounce_value(unsigned int debounce_time) {
-		if (debounce_time > MAX_DEBOUNCE_TIME) debounce_time = MAX_DEBOUNCE_TIME;
-		return (unsigned char)roundf(sqrtf(debounce_time/0.115f));
+	if (debounce_time > MAX_DEBOUNCE_TIME) debounce_time = MAX_DEBOUNCE_TIME;
+	return (unsigned char)roundf(sqrtf(debounce_time / 0.115f));
 }
 
